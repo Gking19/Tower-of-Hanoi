e709335c735c9b3b5d9ee8ceb13d5f3e1b4791fb
Gking19 <gcgking897@gmail.com>
2016-03-10 09:43:19 -0800

    Created project
    Made barebone disc

---
+++ Tower of Hanoi/disc.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class disk here.
+ * 
+ * @author Gary Carlson
+ * @version v1
+ */
+public class disc
+{
+    private int size;
+    public disk(int discSize){
+        size = discSize;
+    }
+}
